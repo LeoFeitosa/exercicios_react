c9f383a9653c56f73e8ebc27c412bac9deb7096f
Leonardo Feitosa <leonardo.feitosa@uniesp.edu.br>
2018-05-16 16:11:57 -0300

    componentes recebendo parametros de componentes filhos

--- ex/component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Primeiro = props => (
-  <h1>Primeiro componente 1</h1>
-)
-
-const Segundo = props => <h1>Segundo componente 2</h1>
-
-export { Primeiro, Segundo }
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Primeiro, Segundo } from './component'
+import Family from './family'
+import Member from './member'
 
 ReactDOM.render(
   <div>
-    <Primeiro />
-    <Segundo />
+    <Family lastName="Silva">
+      <Member name="Leo" />
+      <Member name="João" />
+      <Member name="Maria" />
+    </Family>
   </div>,
   document.getElementById('app'))
